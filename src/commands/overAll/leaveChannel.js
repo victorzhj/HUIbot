eec8028ea034813bc4a444db05b780346bc088bd
Victor Zhou <victor.zhoujiang@tuni.fi>
2023-08-23 17:18:40 +0300

    finished leave command

--- src/commands/overAll/leaveChannel.js
+++ src/commands/overAll/leaveChannel.js
@@ -1,5 +1,15 @@
+const { getVoiceConnection } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('discord.js');
 
-const leave = new SlashCommandBuilder()
-            .setName('leave')
-            .setDescription('Leave the voice channel');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('leave')
+        .setDescription('Leave the voice channel'),
+        async execute(interaction) {
+            const connection = getVoiceConnection(interaction.guild.id);
+            connection.destroy();
+            interaction.reply('Left the voice channel');
+        }
+}
+
+            
